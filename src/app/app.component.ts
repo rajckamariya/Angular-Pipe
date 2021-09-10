0c86a41b5898f6045e2933cb68fd7a674383cdc5
rajckamariya <r.c.kamariya@gmail.com>
2021-09-10 14:00:35 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,11 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  appStatus = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('stable');
+    }, 2000);
+  });
   servers = [
     {
       instanceType: 'medium',
